a0b5e27ee512e7091e53de948b6a03c534afa648
quanyan963 <709641706@qq.com>
2020-03-20 03:37:21 +0800

    增加控制界面

--- app/src/main/java/com/txtled/gp_a209/control/mvp/ControlContract.java
+++ app/src/main/java/com/txtled/gp_a209/control/mvp/ControlContract.java
@@ -1,7 +1,11 @@
 package com.txtled.gp_a209.control.mvp;
 
+import android.app.Activity;
+import android.widget.RadioGroup;
+
 import com.txtled.gp_a209.base.BasePresenter;
 import com.txtled.gp_a209.base.BaseView;
+import com.txtled.gp_a209.control.ControlActivity;
 
 /**
  * Created by Mr.Quan on 2020/3/19.
@@ -12,6 +16,8 @@ public interface ControlContract {
     }
 
     interface Presenter extends BasePresenter<View>{
+        void init(String endpoint, Activity activity);
 
+        void sendMqtt(int id);
     }
 }
