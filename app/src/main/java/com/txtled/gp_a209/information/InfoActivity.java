b80d8acad637873fca47c0d88e5ca5eb1c898d6d
quanyan963 <709641706@qq.com>
2020-03-23 23:59:58 +0800

    添加创建事务逻辑，待完善

--- app/src/main/java/com/txtled/gp_a209/information/InfoActivity.java
+++ app/src/main/java/com/txtled/gp_a209/information/InfoActivity.java
@@ -100,6 +100,7 @@ public class InfoActivity extends MvpBaseActivity<InfoPresenter> implements Info
         if (hasChanged){
             this.setResult(OK);
             hasChanged = false;
+
         }
         super.onBackPressed();
     }
